862cf09d3537a837a3feec89697c909909293278
Shaquille <shaquille-etienne@hotmail.com>
2017-02-14 19:27:13 +0000

    Created a service that updates the last seen location

---
+++ GroupProject/Source/GroupProject/Public/AI/BTUpdateEnemyLastSeenLocation.h
@@ -0,0 +1,22 @@
+// Copyright LostGods
+
+#pragma once
+
+#include "BehaviorTree/BTService.h"
+#include "BTUpdateEnemyLastSeenLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUPPROJECT_API UBTUpdateEnemyLastSeenLocation : public UBTService
+{
+	GENERATED_BODY()
+	
+	
+public:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+//	virtual void ReceiveTick(AActor* OwnerActor, float DeltaSeconds) override;
+	
+};
